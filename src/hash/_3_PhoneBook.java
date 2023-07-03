d15f82fdf1613eb0f9e3388fcdab6d2007d25623
DAHAM LEE <mydaham1004@gmail.com>
2023-07-03 11:16:25 +0900

    _3_PhoneBook.java

--- src/hash/_3_PhoneBook.java
+++ src/hash/_3_PhoneBook.java
@@ -8,27 +8,23 @@ public class _3_PhoneBook {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        //더 짧은수가 긴 수의 접두어일 수 있다. -> 같은 자릿수는 비교 불가, 더 긴수는 접두어일 수 없다.
-        HashMap<Integer, List<String>> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        List<String> compareList = new ArrayList<>();
+        for(int i=0; i < phone_book.length; i++){
+            if(phone_book[i].length() != 20) {
+                String num1 = phone_book[i];
 
-        int num=1;
-        while (num <= 20) {
-            map.put(num, list);
-
-            num++;
+                for(int j=0; j < phone_book.length; j++){
+                    if(i != j){
+                        String num2 = phone_book[j];
+                        if(num1.length() < num2.length()){
+                            answer = num2.startsWith(num1);
+                            if(!answer){
+                                return answer;
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-        for(String number : phone_book){
-            int digit = number.length();
-
-            list = map.get(digit);
-            list.add(number);
-            map.replace(digit, list);
-        }
-
-
 
         return answer;
     }
