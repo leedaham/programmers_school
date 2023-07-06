c2156d8f6d9565fad3740ffc77b65493c9d59a0e
dhleeprecursor <dhleeprecursor@gmail.com>
2023-07-06 09:51:00 +0900

    commit

--- src/hash/_3_PhoneBook.java
+++ src/hash/_3_PhoneBook.java
@@ -1,29 +1,14 @@
 package hash;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
 
 public class _3_PhoneBook {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        for(int i=0; i < phone_book.length; i++){
-            if(phone_book[i].length() != 20) {
-                String num1 = phone_book[i];
-
-                for(int j=0; j < phone_book.length; j++){
-                    if(i != j){
-                        String num2 = phone_book[j];
-                        if(num1.length() < num2.length()){
-                            answer = num2.startsWith(num1);
-                            if(!answer){
-                                return answer;
-                            }
-                        }
-                    }
-                }
-            }
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
 
         return answer;
